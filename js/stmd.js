7f45d9d9a982def162d4247203e2911854f011ff
Roland Shoemaker <rolandshoemaker@gmail.com>
2014-09-21 23:27:25 -0700

    helps if you put the ! in the right place...

--- js/stmd.js
+++ js/stmd.js
@@ -1364,11 +1364,11 @@ var outputJSON = function(ast) {
       if (ast.c) astCopy.c = ast.c;
       if (ast.destination) astCopy.destination = ast.destination;
       if (ast.label) astCopy.label = ast.label;
-      if (!ast.open == undefined) astCopy.open = ast.open;
-      if (!ast.last_line_blank == undefined) astCopy.last_line_blank = ast.last_line_blank;
-      if (!ast.start_line == undefined) astCopy.start_line = ast.start_line;
-      if (!ast.start_column == undefined) astCopy.start_column = ast.start_column;
-      if (!ast.end_line == undefined) astCopy.end_line = ast.end_line;
+      if (ast.open != undefined) astCopy.open = ast.open;
+      if (ast.last_line_blank != undefined) astCopy.last_line_blank = ast.last_line_blank;
+      if (ast.start_line != undefined) astCopy.start_line = ast.start_line;
+      if (ast.start_column != undefined) astCopy.start_column = ast.start_column;
+      if (ast.end_line != undefined) astCopy.end_line = ast.end_line;
       if (ast.string_content) astCopy.string_content = ast.string_content;
       if (ast.strings.length > 0) astCopy.strings = ast.strings;
       if (ast.inline_content.length > 0) astCopy.inline_content = ast.inline_content;
@@ -1385,7 +1385,8 @@ var outputJSON = function(ast) {
     }
     return astCopy
   }
-  return JSON.stringify(prepareAST(ast));
+  JSONast = prepareAST(ast)
+  return JSON.stringify(JSONast);
 }
 
 // The DocParser object.
