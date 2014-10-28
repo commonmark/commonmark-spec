ec8c13d8949e0e4bf5a3a72739c9f8c37a6d3b9f
John MacFarlane <jgm@berkeley.edu>
2014-10-27 23:36:20 -0700

    Added links to spec page.

--- make_site_index.sh
+++ make_site_index.sh
@@ -6,13 +6,16 @@ VERSIONS=`cd $SITE; ls -d -1 0.* | sort -r -g`
 
 echo "% CommonMark Spec\n"
 date=`grep '<div class="version">' $SITE/$SPECVERSION/index.html | perl  -pe 's/^.*(\d\d\d\d-\d\d-\d\d).*$/\1/'`
-echo "[**Latest version ($SPECVERSION)** ($date)](/$SPECVERSION/)\n"
+echo "[**Latest version ($SPECVERSION)**](/$SPECVERSION/) ($date)\n"
+echo "[discussion forum](http://talk.commonmark.org/) | "
+echo "[interactive dingus](/dingus.html) | "
+echo "[repository](https://github.com/jgm/CommonMark/)\n"
 echo "Older versions:\n"
 for vers in $VERSIONS
   do
     date=`grep '<div class="version">' $SITE/$vers/index.html | perl  -pe 's/^.*(\d\d\d\d-\d\d-\d\d).*$/\1/'`
     if [ "$vers" != "$SPECVERSION" ]; then
 	perl -p -i -e 's/<div id="watermark">.*?<\/div>/<div id="watermark" style="background-color:black">This is an older version of the spec. For the most recent version, see <a href="http:\/\/spec.commonmark.org">http:\/\/spec.commonmark.org<\/a>.<\/div>/' $SITE/$vers/index.html
-        echo "- [$vers ($date)](/$vers/)"
+        echo "- [$vers](/$vers/) ($date)"
     fi
   done
