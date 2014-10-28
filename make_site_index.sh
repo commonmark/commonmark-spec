2e27dc3c3fef3ff826dd5406d18e31bccc7d6d1f
John MacFarlane <jgm@berkeley.edu>
2014-10-27 23:26:38 -0700

    Add date to index of versions.

--- make_site_index.sh
+++ make_site_index.sh
@@ -5,12 +5,14 @@ SITE=_site
 VERSIONS=`cd $SITE; ls -d -1 0.* | sort -r -g`
 
 echo "% CommonMark Spec\n"
-echo "[**Latest version ($SPECVERSION)**](/$SPECVERSION/)\n"
+date=`grep '<div class="version">' $SITE/$SPECVERSION/index.html | perl  -pe 's/^.*(\d\d\d\d-\d\d-\d\d).*$/\1/'`
+echo "[**Latest version ($SPECVERSION)** ($date)](/$SPECVERSION/)\n"
 echo "Older versions:\n"
 for vers in $VERSIONS
   do
+    date=`grep '<div class="version">' $SITE/$vers/index.html | perl  -pe 's/^.*(\d\d\d\d-\d\d-\d\d).*$/\1/'`
     if [ "$vers" != "$SPECVERSION" ]; then
 	perl -p -i -e 's/<div id="watermark">.*?<\/div>/<div id="watermark" style="background-color:black">This is an older version of the spec. For the most recent version, see <a href="http:\/\/spec.commonmark.org">http:\/\/spec.commonmark.org<\/a>.<\/div>/' $SITE/$vers/index.html
-        echo "- [$vers](/$vers/)"
+        echo "- [$vers ($date)](/$vers/)"
     fi
   done
