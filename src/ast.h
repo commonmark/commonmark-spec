dc7481869a64fb815191ca8fc3200263f8d8f9a7
Nick Wellnhofer <wellnhofer@aevum.de>
2014-11-17 21:26:36 +0100

    Move reference type definitions to reference.h

--- src/ast.h
+++ src/ast.h
@@ -12,24 +12,7 @@
 extern "C" {
 #endif
 
-#define REFMAP_SIZE 16
 #define MAX_LINK_LABEL_LENGTH 1000
-
-struct cmark_reference {
-	struct cmark_reference *next;
-	unsigned char *label;
-	unsigned char *url;
-	unsigned char *title;
-	unsigned int hash;
-};
-
-typedef struct cmark_reference cmark_reference;
-
-struct cmark_reference_map {
-	cmark_reference *table[REFMAP_SIZE];
-};
-
-typedef struct cmark_reference_map cmark_reference_map;
 
 struct cmark_doc_parser {
 	struct cmark_reference_map *refmap;
