eaec1043a4ad4ec7a8453ddccdefb3ad5bde1c78
John MacFarlane <fiddlosopher@gmail.com>
2014-11-20 13:07:04 -0800

    Removed variance from stats.py.

--- bench/stats.py
+++ bench/stats.py
@@ -5,7 +5,7 @@ import statistics
 
 values = [ float(x) for x in sys.stdin.readlines()]
 
-print("mean = %.4f, median = %.4f, stdev = %.4f, variance = %.4f" %
+print("mean = %.4f, median = %.4f, stdev = %.4f" %
     (statistics.mean(values), statistics.median(values),
-      statistics.stdev(values), statistics.variance(values)))
+      statistics.stdev(values)))
 
