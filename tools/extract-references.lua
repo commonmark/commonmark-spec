9ac36f87e445ed230e133d0759a12ec29c06b15e
John MacFarlane <jgm@berkeley.edu>
2016-01-08 13:49:33 -0800

    Got extract-references.lua working.

--- tools/extract-references.lua
+++ tools/extract-references.lua
@@ -1,3 +1,5 @@
+local cmark = require('cmark')
+
 -- extract list of reference definitions from links of the
 -- form [foo](@foo-id)
 
@@ -5,26 +7,27 @@ local trim = function(s)
   return s:gsub("^%s+",""):gsub("%s+$","")
 end
 
-return function(doc, meta, format)
-    local refs = {}
-    local cur, entering, node_type
-    for cur, entering, node_type in walk(doc) do
-        if node_type == NODE_LINK then
-            local url = node_get_url(cur)
-            if url:match("^@") then
-                local ident = url:sub(2)
-                local children = node_first_child(cur)
-                refs[#refs + 1] = '[' ..
-                     trim(render_commonmark(children, OPT_DEFAULT, 0)) ..
-                     ']: #' .. ident
-                node_set_url(cur, '#' .. ident)
-            end
+local inp = io.read("*a")
+
+-- strip off yaml metadata so it doesn't interfere with parsing:
+local _, endyaml = string.find(inp, "[\n\r]+%.%.%.[\n\r]")
+inp = inp:sub(endyaml)
+
+local doc = cmark.parse_string(inp, cmark.OPT_DEFAULT)
+io.write('\n')
+
+local cur, entering, node_type
+for cur, entering, node_type in cmark.walk(doc) do
+    if node_type == cmark.NODE_LINK then
+        local url = cmark.node_get_url(cur)
+        if url:match("^@") then
+            local ident = url:sub(2)
+            local children = cmark.node_first_child(cur)
+            io.write('[' ..
+                 trim(cmark.render_commonmark(children, cmark.OPT_DEFAULT, 0)) ..
+                 ']: #' .. ident .. '\n')
+            cmark.node_set_url(cur, '#' .. ident)
         end
     end
-    -- Now insert the list of references at the end:
-    local refsblock = node_new(NODE_CUSTOM_BLOCK)
-    node_set_on_enter(refsblock, table.concat(refs, '\n'))
-    local lastblock = node_last_child(doc)
-    node_insert_after(lastblock, refsblock)
 end
 
