8ad9ccc47ea91967a37875cc09f03edcf81b15c3
John MacFarlane <jgm@berkeley.edu>
2016-01-08 13:55:54 -0800

    Made extract-references.lua a filter.

--- tools/extract-references.lua
+++ tools/extract-references.lua
@@ -1,33 +1,31 @@
-local cmark = require('cmark')
-
--- extract list of reference definitions from links of the
+-- append list of reference definitions from links of the
 -- form [foo](@foo-id)
+-- modify anchor links
 
 local trim = function(s)
   return s:gsub("^%s+",""):gsub("%s+$","")
 end
 
-local inp = io.read("*a")
+return function(doc, meta, format)
+  local cur, entering, node_type
+  local refs = {}
+  for cur, entering, node_type in walk(doc) do
+      if node_type == NODE_LINK then
+          local url = node_get_url(cur)
+          if url:match("^@") then
+              local ident = url:sub(2)
+              local children = node_first_child(cur)
+              refs[#refs + 1] = '[' ..
+                   trim(render_commonmark(children, OPT_DEFAULT, 0)) ..
+                   ']: #' .. ident
+              node_set_url(cur, '#' .. ident)
+          end
+      end
+  end
 
--- strip off yaml metadata so it doesn't interfere with parsing:
-local _, endyaml = string.find(inp, "[\n\r]+%.%.%.[\n\r]")
-inp = inp:sub(endyaml)
-
-local doc = cmark.parse_string(inp, cmark.OPT_DEFAULT)
-io.write('\n')
-
-local cur, entering, node_type
-for cur, entering, node_type in cmark.walk(doc) do
-    if node_type == cmark.NODE_LINK then
-        local url = cmark.node_get_url(cur)
-        if url:match("^@") then
-            local ident = url:sub(2)
-            local children = cmark.node_first_child(cur)
-            io.write('[' ..
-                 trim(cmark.render_commonmark(children, cmark.OPT_DEFAULT, 0)) ..
-                 ']: #' .. ident .. '\n')
-            cmark.node_set_url(cur, '#' .. ident)
-        end
-    end
+  local refs_node = node_new(NODE_CUSTOM_BLOCK)
+  node_set_on_enter(refs_node, table.concat(refs, '\n'))
+  node_insert_after(node_last_child(doc), refs_node)
+  -- now append to end
 end
 
