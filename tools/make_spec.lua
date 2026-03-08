4f7733364e1c86cf2038a9634f6499e33f7cba85
John MacFarlane <jgm@berkeley.edu>
2026-03-08 10:44:41 +0100

    Fix small bug in make_spec.lua.
    ipairs was being used instead of pairs in checking for duplicate
    ids.

--- tools/make_spec.lua
+++ tools/make_spec.lua
@@ -56,7 +56,7 @@ local extract_references = function(doc)
   end
   -- check for duplicate IDs
   local idents = {}
-  for _,id in ipairs(refs) do
+  for _,id in pairs(refs) do
     if idents[id] then
       warn("duplicate identifier " .. id)
     end
